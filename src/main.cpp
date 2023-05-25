e1f960ef8b00717b9c565f214fca23c8e3f61357
meequrox <117393211+meequrox@users.noreply.github.com>
2023-05-26 00:52:53 +0700

    Move sources to src/

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include "mainwindow.hpp"
-
 #include <QApplication>
 
-int main(int argc, char** argv) {
+#include "mainwindow.hpp"
+
+int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     MainWindow w;
