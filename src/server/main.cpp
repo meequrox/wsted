f66a001b8c8224b4cb9785e83fdef8e0548d2513
meequrox <117393211+meequrox@users.noreply.github.com>
2023-06-01 03:35:07 +0700

    [Server] Set server port

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,7 +1,34 @@
+#include <iomanip>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    std::cout << "wsted server" << std::endl;
+#define DEFAULT_PORT 8044
 
-    return 0;
+void usage(std::string exe) {
+    std::cout << std::left;
+
+    std::cout << "Usage: " << std::endl;
+    std::cout << exe << std::setw(32) << " PORT"
+              << "use custom port (1024-49151)" << std::endl;
+    std::cout << std::setw(32 + exe.size()) << exe << "use default port (" << DEFAULT_PORT << ")"
+              << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    int port;
+
+    if (argc == 1) {
+        port = DEFAULT_PORT;
+    } else if (argc == 2) {
+        auto newPort = std::atoi(argv[1]);
+        port = newPort >= 1024 && newPort <= 49151 ? newPort : DEFAULT_PORT;
+    } else {
+        std::cout << "Too many arguments" << std::endl << std::endl;
+
+        usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "wsted server on 0.0.0.0:" << port << std::endl;
+
+    return EXIT_SUCCESS;
 }
