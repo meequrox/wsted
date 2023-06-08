29a54ed338d8222797f557ef979847fb026219d9
meequrox <117393211+meequrox@users.noreply.github.com>
2023-06-09 00:57:26 +0700

    [Logger] Remove LOG_CALL() macro

--- src/logger.hpp
+++ src/logger.hpp
@@ -6,8 +6,6 @@
 #include <QString>
 #include <QTcpSocket>
 
-#define LOG_CALL() qDebug().nospace() << __PRETTY_FUNCTION__ << " call" << Qt::endl;
-
 void messageLogger(const QString& action, const QString& clientName, const QString& msg);
 void messageLogger(const QString& action, const QTcpSocket* socket, const QString& msg);
 
